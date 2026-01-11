9a873e68e8e70f65fd85e1b209687511170517ab
UD <upendra.mishra@rockwellautomation.com>
2026-01-11 23:12:27 +0530

    Initial Cypress project setup with test configuration

---
+++ cypress/integrations/examples/Test1.js
@@ -0,0 +1,12 @@
+// Cypress -- Spec file / Test File
+
+describe('Navigating to Web Application', function()
+{
+it('Navigate to URL', function()
+{
+// test step1
+cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
+
+})
+
+})
